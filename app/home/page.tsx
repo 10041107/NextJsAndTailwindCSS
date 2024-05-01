2edcc2629d0b92be1c5f3bc1109a7fdbcb5661ec
10041108 <aksmfdyfl111@gmail.com>
2024-05-01 10:36:46 +0900

    page Additional Test

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        home Portfolio
+      </h1>
+      <div className="my-8">
+      </div>
+    </section>
+  )
+}
